a1050e04af4d364c8528e6664a27e631788a6e42
Brian Burns <bdburns6389@gmail.com>
2020-12-05 22:48:03 -0600

    Added mongoose models

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
+const User = require("./models/user");
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nalyi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -12,12 +18,41 @@ app.use(
   graphqlHTTP({
     graphiql: true,
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            creator: User!
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+            createdEvents: [Event!]
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput):  Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -27,14 +62,82 @@ app.use(
         `),
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "Sailing", "All-night Coding"];
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5fcbd147f0e9540ec4794a86",
+        });
+
+        let createdEvent;
+
+        return event
+          .save()
+          .then((result) => {
+            createdEvent = { ...result._doc, _id: result.id };
+            return User.findById("5fcbd147f0e9540ec4794a86");
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error("User not found");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            return createdEvent;
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error("User already exists");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
     },
   })
 );
 
-app.listen(4000);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
