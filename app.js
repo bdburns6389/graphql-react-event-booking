246b6a18a43cc84b9a8e373bf598e4cc60186ff0
Brian Burns <bdburns6389@gmail.com>
2020-12-15 01:57:25 -0600

    Added modal

--- app.js
+++ app.js
@@ -12,25 +12,37 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	}
+
+	next();
+});
+
 app.use(isAuth);
 
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    graphiql: true,
-    schema: graphqlSchema,
-    rootValue: graphqlResolvers,
-  })
+	"/graphql",
+	graphqlHTTP({
+		graphiql: true,
+		schema: graphqlSchema,
+		rootValue: graphqlResolvers,
+	})
 );
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(4000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(4000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+import "./Events.css";
 
 class EventsPage extends Component {
+	state = {
+		creating: false,
+	};
+
+	modalConfirmHandler = () => {
+		this.setState({ creating: false });
+	};
+
+	modalCancelHandler = () => {
+		this.setState({ creating: false });
+	};
+
+	startCreateEventHandler = () => {
+		this.setState({ creating: true });
+	};
+
 	render() {
-		return <div>The Events Page</div>;
+		return (
+			<React.Fragment>
+				{this.state.creating && <Backdrop />}
+				{this.state.creating && (
+					<Modal
+						title="Add Event"
+						canCancel
+						canConfirm
+						onCancel={this.modalCancelHandler}
+						onConfirm={this.modalConfirmHandler}
+					>
+						<p>Modal Content</p>
+					</Modal>
+				)}
+
+				<div className="events-control">
+					<p>Share your own events!</p>
+					<button className="btn" onClick={this.startCreateEventHandler}>
+						Create Event
+					</button>
+				</div>
+			</React.Fragment>
+		);
 	}
 }
 
